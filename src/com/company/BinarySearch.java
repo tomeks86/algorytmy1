d908c1663a73e0d36f638d706f25e140ccdf7292
Tomasz S <tomasz.z.seidler@gmail.com>
2017-09-11 21:16:42 +0200

    binary search tests moved to BinarySearch class

--- src/com/company/BinarySearch.java
+++ src/com/company/BinarySearch.java
@@ -52,30 +52,34 @@ public class BinarySearch implements SearchAlgorithm {
         } else return -1;
     }
 
-    /*@Override
-    public int searchDoublesReverted(int[] T, int x) {
+    public static void main(String[] args) {
 
-        int L = 0, c, P = T.length;
-        if (T.length == 0) {
-            System.out.println("empty table provided...");
-            return -1;
+        int size = 30;
+        int[] exTable = new int[size];
+        for (int i = 0; i < size; i++) {
+            exTable[i] = i / 8;
         }
 
-        P -= 1;
+        int[] exTable2 = {0, 1, 1, 2, 5, 6, 7, 8, 8, 8, 9, 9, 9, 9, 9};
 
-        while(L < P) {
-            c = (L + P + 1) / 2;
-            if (T[c] > x) {
-                P = c - 1;
+        BinarySearch binarySearch = new BinarySearch();
+        binarySearch.search(exTable, 5);
+        int search = 3;
+        int index = binarySearch.search(exTable2, search);
+        System.out.println(index);
+        //int index = binarySearch.search(exTable, search);
+        if (index > -1) System.out.println(index + " " + exTable[index]);
+        else System.out.println("Element: " + search + " not found!");
+        index = binarySearch.search2(exTable, search);
+        if (index > -1) System.out.println(index + " " + exTable[index]);
+        else System.out.println("Element: " + search + " not found!");
+        for(int i = 0; i < 20; i++) {
+            index = binarySearch.search(exTable, i);
+            if (index > -1) {
+                System.out.println("element " + i + " found at index: " + index + " (check): " + exTable[index]);
             } else {
-                L = c;
+                System.out.println("element " + i + " not found...");
             }
         }
-
-        if (T[L] == x) {
-            return L;
-        } else return -1;
-    }*/
-
-
+    }
 }
