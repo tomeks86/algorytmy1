d51318647d2359e0506bd02ad7cea9e754041c59
Tomasz S <tomasz.z.seidler@gmail.com>
2017-09-16 12:48:37 +0200

    sqrt method -> sqrtNewton and new sqrt method is based on bubble sort algorithm

--- src/com/company/Math.java
+++ src/com/company/Math.java
@@ -2,10 +2,36 @@ package com.company;
 
 public class Math {
 
-    public static double sqrt(double r, double e) {
+    public static double pow(double base, int exp) {
+        if (exp < 0) {
+            return -1;
+        }
+        double res = 1;
+        int r;
+        while (exp > 0) {
+            r = exp % 2;
+            exp /= 2;
+            if (r > 0) {
+                res *= base;
+                //res %= 1000;    //return only last 3 digits...
+            }
+            base *= base;
+        }
+        return res;
+    }
+
+    public static double sqrtNewton(double r, double e) {
         if (r < 0) {
             System.out.println("negative numbers not implemented!");
             return -1;
+        } else if (r < 1) {
+            int n = 0;
+            double r2 = r;
+            while (r2 < 1) {
+                r2 *= 10;
+                n += 1;
+            }
+            return sqrtNewton(r2,e)/sqrtNewton(pow(10.,n),e);
         } else {
             double xn = r, xnp1 = .5 * (r / xn + xn);
             while ((xn - xnp1) > e) {
@@ -16,10 +42,29 @@ public class Math {
         }
     }
 
+    public static double sqrt(double r, double e) {
+        if (r < 0) {
+            System.out.println("negative numbers not implemented!");
+            return -1;
+        }
+        double L = 0, P = r, c;
+        while (P-L > e) {
+            c = (P + L) / 2;
+            if (c*c > r) {
+                P = c;
+            } else {
+                L = c;
+            }
+        }
+        return (L + P) / 2;
+    }
+
     public static void main(String[] args) {
 
+        System.out.println(sqrtNewton(2,1.e-6));
         System.out.println(sqrt(2,1.e-6));
-        System.out.println(sqrt(-0.002,1.e-3));
+        System.out.println(sqrtNewton(0.000000000016,1.e-10));
+        System.out.println(sqrtNewton(2000,1.e-3));
         System.out.println(sqrt(2000,1.e-3));
         //System.out.println(sqrt(2,1.e-1) - sqrt(2,1e-4));
     }
